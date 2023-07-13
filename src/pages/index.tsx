e673e29004161c2a03c047da30f1cab58ba119ab
gracebir <grace.devolop@gmail.com>
2023-07-13 14:57:04 +0200

    home cleaning up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main>
-      
+      <h1>Home page</h1>
     </main>
   )
 }
