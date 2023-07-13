89262d7ce8280cae5e6133247e6066cc7a45315f
gracebir <grace.devolop@gmail.com>
2023-07-13 14:43:02 +0200

    login screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Quicksand } from 'next/font/google'
+
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  weight: ['500', '600', '700']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`bg-very-dark min-h-screen text-white ${quicksand.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,20 @@
+import LoginForm from '@/components/LoginForm'
+import Link from 'next/link'
 import React from 'react'
 
 function Login() {
   return (
-    <div>
-      
+    <div className='flex items-center h-screen'>
+        <div className="max-w-4xl mx-auto grid sm:grid-cols-1 lg:grid-cols-2 gap-4 border border-dark-color rounded-md min-h-[50vh] flex-1 p-4">
+          <LoginForm/>
+          <div className="bg-blue-color text-white-color hidden lg:flex flex-col items-center justify-center rounded-md h-full lg:col-span-1">
+            <div className='text-center px-3 flex flex-col gap-4 items-center'>
+              <h3 className="text-2xl font-bold">Welcome to e-store</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+              <Link className='px-6 py-2 rounded-md border border-white-color' href={"/signup"}>Sign Up</Link>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
