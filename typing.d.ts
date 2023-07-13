47bd001b0deba01daec3ae977abfb3753db1e575
gracebir <grace.devolop@gmail.com>
2023-07-13 21:07:17 +0200

    dropdown component

--- typing.d.ts
+++ typing.d.ts
@@ -1,9 +1,17 @@
 export type tInputType = {
     type: string
     placeholder: string
-    onChange: string
-    onBlur: string
+    onChange: React.ChangeEventHandler<HTMLInputElement>
+    onBlur?: React.FocusEventHandler<HTMLInputElement>
     value: string | number
     name: string
-    errorMessage?: string
+    errorMsg?: string
+    label: string
+}
+
+export type tDropdrownProps = {
+    placeholder: string
+    setPlaceholder: React.Dispatch<React.SetStateAction<string>>
+    data: Array<string>
+    typeDrop: string
 }
