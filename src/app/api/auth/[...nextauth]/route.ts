9b200c96fb23e74b9c07aac60ff97a680be2972c
gracebir <grace.devolop@gmail.com>
2023-07-18 12:55:42 +0200

    setting out the authentication with next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Email", type: "text", placeholder: "email address" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const res = await fetch('http://localhost:3000/api/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username: credentials?.username,
+                        password: credentials?.password
+                    })
+                })
+
+                const user = await res.json()
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            return { ...token, ...user }
+        },
+        async session({ session, token }) {
+            session.user = token as any
+            return session
+        }
+    },
+    pages: {
+        signIn: "/login"
+    }
+});
+
+
+export { handler as GET, handler as POST }
