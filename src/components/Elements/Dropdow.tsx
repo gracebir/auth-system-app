e3965ece320f3e47fe8d31c7aa05702656ff14f3
gracebir <grace.devolop@gmail.com>
2023-07-19 23:54:13 +0200

    dropdown element

--- src/components/Elements/Dropdow.tsx
+++ src/components/Elements/Dropdow.tsx
@@ -1,16 +1,18 @@
+import { tDropdrownProps } from '@/lib/type'
 import React, { useState } from 'react'
 import { BsChevronDown, BsChevronUp } from 'react-icons/bs'
 
-const Dropdown = () => {
+const Dropdown = ({ text, isStart = false, data, setText }: tDropdrownProps) => {
     const [open, setOpen] = useState(false)
     const handleDropdownText = (i: number) => {
         setOpen(false)
+        setText(data[i])
     }
     return (
         <div className="relative flex w-full">
             <div onClick={() => setOpen(!open)} className="bg-blue-color px-3 py-2 cursor-pointer flex items-center w-full justify-between rounded-sm">
                 <div className='flex gap-2 items-center'>
-                    <span className='font-semibold'>Monday</span>
+                    {text ? <span className='font-semibold'>{text}</span>: isStart ? <span className='font-semibold italic text-gray-500'>Monday</span>:<span className='font-semibold italic text-gray-500'>Wednesday</span>}
                 </div>
                 {open ? <BsChevronUp /> : <BsChevronDown />}
             </div>
@@ -20,7 +22,11 @@ const Dropdown = () => {
                         <span className="font-bold">Times</span>
                     </div>
                     <div className='flex flex-col gap-6 px-4 py-4 h-[300px] overflow-y-scroll'>
-                        
+                        {data.map((item, key) => (
+                            <div onClick={() => handleDropdownText(key)} key={key} className='flex items-center cursor-pointer justify-between'>
+                                <p>{item}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             )}
